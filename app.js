e0fd657accd28bbfa7a84500543513375ce8957d
Florin Pop <popflorin1705@yahoo.com>
2017-02-03 00:16:01 +0200

    tested ball, all works

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 const bodyParser = require('body-parser');
+const Ball = require('./lib/ball');
 
 const PORT = process.env.PORT || 3000;
 
+let canvasWidth = 800;
+let canvasHeight = 600;
+
 let connections = [];
 let users = [];
-let ball = new Ball();
+let ball = new Ball(canvasWidth, canvasHeight);
+
+setInterval(tick, 1000);
+
+function tick() {
+    
+}
+
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
